118ea8be7de812ecd1564f98b3a967d051f350f9
Frank Rauen <home@Franks-MacBook-Air.local>
2021-02-16 09:21:47 -0700

    adds LocationShareData file and skeleton

---
+++ projecttwo/src/main/scala/projecttwo/LocationShareData.scala
@@ -0,0 +1,20 @@
+package projecttwo
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions
+import org.apache.spark.sql.DataFrameReader
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.client.config.CookieSpecs
+import org.apache.http.client.utils.URIBuilder
+import org.apache.http.client.methods.HttpGet
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.nio.file.Files
+import java.nio.file.Paths
+import scala.concurrent.Future
+
+object LocationShareData {
+    
+}
