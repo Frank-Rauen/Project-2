455b958b98e6bc82941d08ac65027ad0c26a6320
Frank Rauen <home@Franks-MacBook-Air.local>
2021-02-16 10:01:38 -0700

    adds LocationShareData boilerplate

--- projecttwo/src/main/scala/projecttwo/LocationShareData.scala
+++ projecttwo/src/main/scala/projecttwo/LocationShareData.scala
@@ -16,5 +16,120 @@ import java.nio.file.Paths
 import scala.concurrent.Future
 
 object LocationShareData {
-    
+    def demoTrack(spark: SparkSession)
+    {
+        helloTweetStream(spark)
+    }
+
+    def helloTweetStream(spark: SparkSession): Unit = {
+    import spark.implicits._
+
+    //grab a bearer token from the environment
+    //never hardcode your tokens (never just put them as a string in your code)
+    val bearerToken = System.getenv(("TWITTER_BEARER_TOKEN"))
+
+    //writes all the tweets from twitter's stream into a directory
+    // by default hits the sampled stream and uses "twitterstream" as the directory
+    // We'll run it in the background using a Future:
+    // We're not saving a reference to this future or providing a callback function
+    // we just start it running in the background and forget about it.
+    import scala.concurrent.ExecutionContext.Implicits.global
+    Future {
+      tweetStreamToDir(bearerToken, queryString = "?tweet.fields=geo,created_at&expansions=geo.place_id")
+    }
+
+    //Here we're just going to wait until a file appears in our twitterstream directory
+    // or until some reasonable amount of time has passed (30s)
+    var start = System.currentTimeMillis()
+    var filesFoundInDir = false
+    while(!filesFoundInDir && (System.currentTimeMillis()-start) < 30000) {
+      filesFoundInDir = Files.list(Paths.get("twitterstream1")).findFirst().isPresent()
+      Thread.sleep(500)
+    }
+    if(!filesFoundInDir) {
+      println("Error: Unable to populate tweetstream after 30 seconds.  Exiting..")
+      System.exit(1)
+    }
+
+    //We're going to start with a static DF
+    // both to demo it, and to infer the schema
+    // streaming dataframes can't infer schema
+    val staticDf = spark.read.json("StaticLocationShareData")
+
+    staticDf.printSchema()
+
+    //streamDf is a stream, using *Structured Streaming*
+    val streamDf1 = spark.readStream.schema(staticDf.schema).json("StaticLocationShareData")
+    val streamDf2 = spark.readStream.schema(staticDf.schema).json("StaticLocationShareData")
+
+    // streamDf1
+    //     .filter(functions.isnull($"includes.places"))
+    //   .select(functions.hour($"data.created_at").as("hour"))
+    //   .groupBy("hour")
+    //   .count()
+    //   .writeStream
+    //   .outputMode("complete")
+    //   .format("console")
+    //   .start()
+    //   .awaitTermination()
+
+    streamDf2
+        .filter(!functions.isnull($"includes.places"))
+      .select(functions.hour($"data.created_at").as("hour"))
+      .groupBy("hour")
+      .count()
+      .writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+      .awaitTermination()
+  }
+
+
+
+    def tweetStreamToDir(
+      bearerToken: String,
+      dirname: String = "LocationShareDataTweetStream",
+      linesPerFile: Int = 1000,
+      queryString: String = ""
+  ) = {
+    //a decent chunk of boilerplate -- from twitter docs/tutorial
+    //sets up the request we're going to be sending to Twitter
+    val httpClient = HttpClients.custom
+      .setDefaultRequestConfig(
+        RequestConfig.custom.setCookieSpec(CookieSpecs.STANDARD).build()
+      )
+      .build()
+    val uriBuilder: URIBuilder = new URIBuilder(
+      s"https://api.twitter.com/2/tweets/sample/stream$queryString"
+    )
+    val httpGet = new HttpGet(uriBuilder.build())
+    //set up the authorization for this request, using our bearer token
+    httpGet.setHeader("Authorization", s"Bearer $bearerToken")
+    val response = httpClient.execute(httpGet)
+    val entity = response.getEntity()
+    if (null != entity) {
+      val reader = new BufferedReader(
+        new InputStreamReader(entity.getContent())
+      )
+      var line = reader.readLine()
+      //initial filewriter, replaced every linesPerFile
+      var fileWriter = new PrintWriter(Paths.get("tweetstream.tmp").toFile)
+      var lineNumber = 1
+      val millis = System.currentTimeMillis() //get millis to identify the file
+      while (line != null) {
+        if (lineNumber % linesPerFile == 0) {
+          fileWriter.close()
+          Files.move(
+            Paths.get("tweetstream.tmp"),
+            Paths.get(s"$dirname/tweetstream-$millis-${lineNumber/linesPerFile}"))
+          fileWriter = new PrintWriter(Paths.get("tweetstream.tmp").toFile)
+        }
+        fileWriter.println(line)
+        line = reader.readLine()
+        lineNumber += 1
+      }
+
+    }
+  }
 }
